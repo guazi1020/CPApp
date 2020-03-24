06e93210b776730a65e2fe8a7c31f14e7fbe1e6e
guazi1020 <guazi1020@yeah.net>
2020-03-24 13:21:12 +0800

    0324

--- src/test.js
+++ src/test.js
@@ -4,7 +4,8 @@ class RequestStu extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            datas: []
+            datas: {},
+            arr: []
         }
     }
 
@@ -14,29 +15,30 @@ class RequestStu extends React.Component {
             mode: "cors",
             credentials: "include",
             redirect: 'follow',
-            headers:{
-                'Accept':'application/json,text/plain,*/*'
+            headers: {
+                'Accept': 'application/json,text/plain,*/*'
             }
-        }).then(response=>response.text())
-        .then(result=>{
-            this.setState({
-                datas:result
-            },function(){
-                console.log(this.state.datas)
-           
-                // let com=this.state.test.retBody.map((item,index)=>{
-                //     console.log(item.CreateDate)
-                // })
+        }).then(response => response.json())
+            .then((data) => {
+                this.setState({
+                    datas: data
+                }, function () {
+                    console.log(this.state.datas)
+                    let com = this.state.datas.map((item, index) => {
+                        console.log(item.game)
+                        return <li key={index}>{item.game}</li>
+                    })
+                    this.setState({
+                        arr: com
+                    })
+                }
+                )
             })
-        })
-        .catch(e=>{console.log("err")})
+            .catch(e => { console.log("err") })
 
-       //console.log(this.state.test)
     }
     render() {
-        return (
-        <div></div>
-        )
+        return <div>{this.state.arr}</div>
     }
 }
 export default RequestStu
